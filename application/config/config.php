18e224a18359e063e7aea2b09f1df343c35fa29d
fatihcerci <58953105+fatihcerci@users.noreply.github.com>
2022-07-25 23:23:53 +0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -388,7 +388,7 @@ $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
 
-$config['session_expire_add_time'] = '+60 sec';
+$config['session_expire_add_time'] = '+600 sec';
 
 
 /*
